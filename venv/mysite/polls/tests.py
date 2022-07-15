7dac4e01045b3e92a4659c63a3344bc7f85e2898
jung2uncan <jung2uncan@gmail.com>
2022-07-15 17:29:39 +0900

    tests.py 구성하기

--- venv/mysite/polls/tests.py
+++ venv/mysite/polls/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
+
+class QuestionModelTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is in the future.
+        """
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1) # time은 현재 시간에서 1일 1초 전 시간 (음수)
+        old_question= Question(pub_date=time)
+
+        # self.assertIs(테스트 하고픈 조건, 기대하는 Boolean값)
+        self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        """
+         was_published_recently() returns True for questions whose pub_date
+         is within the last day.
+        """
+
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59) # time은 현재 시간에서 23시간 59분 59초 전 시간 (양수)
+        recent_question = Question(pub_date=time)
+
+        self.assertIs(recent_question.was_published_recently(), True)
+
